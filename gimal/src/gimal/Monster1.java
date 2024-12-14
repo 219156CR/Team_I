22026fc5db2be043c149a6277e2c5f9e14d905b8
kejiuumin <dmlwns71@gmail.com>
2024-12-14 13:54:39 +0900

    2024-12-14 게임 몬스터 gif 나타내기

--- gimal/src/gimal/Monster1.java
+++ gimal/src/gimal/Monster1.java
@@ -1,25 +1,128 @@
 package gimal;
 
 import java.awt.Graphics;
-import java.awt.Rectangle;
+import java.util.Random;
+import javax.swing.ImageIcon;
 
 public class Monster1 {
-    private int x = 450;
-    private int y = 450;
-    private int width = 90;
-    private int height = 105;
-    
+    private ImageIcon monsterIdleGif;
+    private ImageIcon monsterMoveGif;
+    private ImageIcon[] monsterAttackGifs;  // 공격 GIF 배열
+    private ImageIcon monsterDeadGif;  // 죽음 GIF
+    private int x = 1400;  // 시작 지점을 오른쪽으로 설정
+    private int y = 650;
+    private double speed = 0.1;  // 속도를 0과 1 사이로 설정
+    private final int groundY = 650;
+
+    private String currentState = "MOVE";  // 상태를 추적하는 변수 (이동 상태 기본값)
+
+    private boolean isDead = false;  // 몬스터가 죽었는지 여부
+
+    public Monster1() {
+        monsterIdleGif = new ImageIcon("IMAGE/모래시계 기본.gif");
+        monsterMoveGif = new ImageIcon("IMAGE/모래시계 기분좋음.gif");
+        // 공격 GIF 여러 개를 배열로 저장
+        monsterAttackGifs = new ImageIcon[] {
+            new ImageIcon("IMAGE/모래시계 공격1.gif"),
+            new ImageIcon("IMAGE/모래시계 공격2.gif")
+        };
+        monsterDeadGif = new ImageIcon("IMAGE/모래시계 죽음.gif");  // 죽음 GIF
+
+        this.y = groundY;
+    }
+
     public void draw(Graphics g, Screen screen) {
-        // 몬스터 그리기 로직 구현
-        g.fillRect(x, y, width, height);
+        if (isDead) {
+            g.drawImage(monsterDeadGif.getImage(), x, y, screen);  // 죽었을 때
+        } else if (currentState.equals("IDLE")) {
+            g.drawImage(monsterIdleGif.getImage(), x, y, screen);  // 정지 상태
+        } else if (currentState.equals("MOVE")) {
+            g.drawImage(monsterMoveGif.getImage(), x, y, screen);  // 이동 상태
+        } else if (currentState.equals("ATTACK")) {
+            // 공격 상태일 때 랜덤으로 공격 GIF 선택
+            Random rand = new Random();
+            int randomIndex = rand.nextInt(monsterAttackGifs.length);  // 0 또는 1 랜덤으로 선택
+            g.drawImage(monsterAttackGifs[randomIndex].getImage(), x, y, screen);  // 랜덤 공격 GIF
+        }
     }
 
-    public void setPosition(int x, int y) {
-        this.x = x;
-        this.y = y;
+    public void move() {
+        if (currentState.equals("MOVE")) {
+            x += speed;  // speed에 따라 오른쪽(양수) 또는 왼쪽(음수)으로 이동
+            
+            // 왼쪽 벽에 부딪히면 오른쪽으로 이동
+            if (x < 0) {
+                x = 0;  
+                speed = Math.abs(speed);  // 오른쪽으로 이동
+            } 
+            // 오른쪽 벽에 부딪히면 왼쪽으로 이동
+            else if (x > 1470) {
+                x = 1470;
+                speed = -Math.abs(speed);  // 왼쪽으로 이동
+            }
+        }
     }
 
-    public Rectangle getRect() {
-        return new Rectangle(x, y, width, height);
+    public void randomizeState() {
+        Random rand = new Random();
+        int randomState = rand.nextInt(2);  // 0 또는 1을 랜덤으로 선택
+        int randomDuration = rand.nextInt(3000);  // 0초에서 3초 사이의 랜덤 시간
+
+        if (!isDead) {  // 몬스터가 죽지 않았다면
+            if (randomState == 0) {
+                currentState = "IDLE";  // 정지 상태
+            } else {
+                currentState = "MOVE";  // 이동 상태
+            }
+            try {
+                Thread.sleep(randomDuration);  // 랜덤 시간 동안 정지 또는 이동
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // 게임 루프에서 주기적으로 호출되는 메서드
+    public void gameLoop() {
+        while (!isDead) {
+            randomizeState();  // 상태 랜덤화
+            move();  // 이동 호출
+            // 화면에 몬스터 그리기 (그래픽 객체가 필요합니다)
+            // 예: monster.draw(g, screen);
+            try {
+                Thread.sleep(100);  // 100ms 마다 상태 변경
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // 캐릭터와 몬스터 간의 거리를 계산해 공격을 시작하는 메서드
+    public void checkAttackCondition(int characterX, int characterY) {
+        // 캐릭터와 몬스터 간의 거리를 계산
+        int distance = (int) Math.sqrt(Math.pow(x - characterX, 2) + Math.pow(y - characterY, 2));
+
+        // 일정 거리 이내에 캐릭터가 오면 공격 상태로 전환
+        if (distance < 100) {  // 100은 임의의 범위
+            currentState = "ATTACK";  // 공격 상태로 전환
+        } else {
+            currentState = "MOVE";  // 공격 범위를 벗어나면 이동 상태로 전환
+        }
+    }
+
+    // 몬스터가 죽었을 때 상태 변경
+    public void die() {
+        isDead = true;  // 몬스터 죽음
+        currentState = "IDLE";  // 죽으면 정지 상태로 설정
+    }
+
+    // 몬스터가 살아있는지 확인
+    public boolean isDead() {
+        return isDead;
+    }
+
+    // 몬스터가 이동 상태로 시작하는 메서드
+    public void startMoving() {
+        currentState = "MOVE";
     }
 }
--- gimal/src/gimal/Screen.java
+++ gimal/src/gimal/Screen.java
@@ -11,112 +11,117 @@ import java.util.TimerTask;
 import javax.swing.ImageIcon;
 
 public class Screen extends Canvas implements ComponentListener {
-	
-	private Graphics bg;
-	private Image offScreen;
-	private Image backgroundImage;
-	private Dimension dim;
-	private Character1 ryu1 = new Character1();
+    
+    private Graphics bg;
+    private Image offScreen;
+    private Image backgroundImage;
+    private Dimension dim;
+    private Character1 ryu1 = new Character1();
     private Character2 ryu2 = new Character2();
     private Character3 ryu3 = new Character3();
-	private int countNumber = 0;
-	
-	public Screen() {
-		addComponentListener(this);
-		// 배경 이미지 로드
-		backgroundImage = new ImageIcon("IMAGE/필드 배경1.JPG").getImage();
-		
-		// 선택된 캐릭터에 따라 키 리스너 추가
-		switch (Cchoise.getSelectedCharacter()) {
-			case 1:
-				addKeyListener(ryu1);
-				break;
-			case 2:
-				addKeyListener(ryu2);
-				break;
-			case 3:
-				addKeyListener(ryu3);
-				break;
-			default:
-				// 선택된 캐릭터가 없을 경우 처리
-				break;
-		}
-		setFocusable(true);
-		Timer timer = new Timer();
-		timer.schedule(new TimerTask() {
-			
-			@Override
-			public void run() {
-				repaint();
-				counting();
-			}
-		}, 0, 1);
-	}
-	public void counting() {
-		this.countNumber++;
-	}
-	
-	public int getCount() {
-		return this.countNumber;
-	}
-	
-	private void initBuffer() {
-		this.dim = getSize();
-		this.offScreen = createImage(dim.width, dim.height);
-		this.bg = this.offScreen.getGraphics();
-	}
-	
-	@Override
-	public void paint(Graphics g) {
-		bg.clearRect(0, 0, dim.width, dim.height);
-		// 배경 이미지 그리기
-		bg.drawImage(backgroundImage, 0, 0, dim.width, dim.height, this);
-		
-		// 캐릭터 선택에 따라 그리기
-		switch (Cchoise.getSelectedCharacter()) {
-			case 1:
-				ryu1.draw(bg, this);
-				break;
-			case 2:
-				ryu2.draw(bg, this);
-				break;
-			case 3:
-				ryu3.draw(bg, this);
-				break;
-			default:
-				// 선택된 캐릭터가 없을 경우 처리
-				break;
-		}
-		g.drawImage(offScreen, 0, 0, this);
-	}
-	
-	@Override
-	public void update(Graphics g) {
-		paint(g);
-	}
-	
-	@Override
-	public void componentResized(ComponentEvent e) {
-		// TODO Auto-generated method stub
-		initBuffer();
-	}
+    private Monster1 monster1 = new Monster1();
+    private int countNumber = 0;
+    
+    public Screen() {
+        addComponentListener(this);
+        // 배경 이미지 로드
+        backgroundImage = new ImageIcon("IMAGE/필드 배경1.JPG").getImage();
+        
+        // 선택된 캐릭터에 따라 키 리스너 추가
+        switch (Cchoise.getSelectedCharacter()) {
+            case 1:
+                addKeyListener(ryu1);
+                break;
+            case 2:
+                addKeyListener(ryu2);
+                break;
+            case 3:
+                addKeyListener(ryu3);
+                break;
+            default:
+                // 선택된 캐릭터가 없을 경우 처리
+                break;
+        }
+        setFocusable(true);
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            
+            @Override
+            public void run() {
+                repaint();
+                counting();
+                monster1.move(); // 몬스터 움직임 업데이트
+            }
+        }, 0, 1);
+    }
+    
+    public void counting() {
+        this.countNumber++;
+    }
+    
+    public int getCount() {
+        return this.countNumber;
+    }
+    
+    private void initBuffer() {
+        this.dim = getSize();
+        this.offScreen = createImage(dim.width, dim.height);
+        this.bg = this.offScreen.getGraphics();
+    }
+    
+    @Override
+    public void paint(Graphics g) {
+        if (bg == null) {
+            initBuffer();
+        }
+        bg.clearRect(0, 0, dim.width, dim.height);
+        // 배경 이미지 그리기
+        bg.drawImage(backgroundImage, 0, 0, dim.width, dim.height, this);
+        
+        // 캐릭터 선택에 따라 그리기
+        switch (Cchoise.getSelectedCharacter()) {
+            case 1:
+                ryu1.draw(bg, this);
+                break;
+            case 2:
+                ryu2.draw(bg, this);
+                break;
+            case 3:
+                ryu3.draw(bg, this);
+                break;
+            default:
+                // 선택된 캐릭터가 없을 경우 처리
+                break;
+        }
+        
+        // 몬스터 그리기
+        monster1.draw(bg, this);
+        
+        g.drawImage(offScreen, 0, 0, this);
+    }
+    
+    @Override
+    public void update(Graphics g) {
+        paint(g);
+    }
+    
+    @Override
+    public void componentResized(ComponentEvent e) {
+        initBuffer();
+    }
 
-	@Override
-	public void componentMoved(ComponentEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void componentMoved(ComponentEvent e) {
+        // TODO Auto-generated method stub
+    }
 
-	@Override
-	public void componentShown(ComponentEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void componentShown(ComponentEvent e) {
+        // TODO Auto-generated method stub
+    }
 
-	@Override
-	public void componentHidden(ComponentEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
+    @Override
+    public void componentHidden(ComponentEvent e) {
+        // TODO Auto-generated method stub
+    }
 }
